dab71e1708470100e8aff35787f422f5c7c890c5
Frank Lavoie <lav.francis@gmail.com>
2014-02-12 23:41:01 -0500

    removed print statement

--- EKG4Test/listener.py
+++ EKG4Test/listener.py
@@ -24,23 +24,18 @@ def get_socket():
                                                               socket.AF_UNSPEC, socket.SOCK_STREAM,
                                                               0, socket.AI_PASSIVE)[0]
     try:
-        print("trying")
         sckt = socket.socket(af, socktype, proto)
         sckt.bind(sa)
         return sckt.listen(1)
     except socket.error, msg:
         sckt.close()
-        print 'could not open socket'
         sys.exit(1)
 
 
 def listening(conn):
     while 1:
-        print("looping")
         data = conn.recv(1024)
-        print("data : %s" % data)
-        if not data:
-            break
+        if not data: return
         conn.send(data)
 
 def connect():
