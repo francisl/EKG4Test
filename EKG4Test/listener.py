0d9abe7e36234353ed3b8ccc8d66c1c8f7099e1b
Frank Lavoie <lav.francis@gmail.com>
2014-02-13 00:02:17 -0500

    MessageParse check when message is completed by a newline

--- EKG4Test/listener.py
+++ EKG4Test/listener.py
@@ -13,12 +13,20 @@ class MessageParser(object):
         self.runner = None
         self.failed = None
         self.succeed = None
+        self.completed = False
 
     def is_valid(self):
         if (self.name is None or self.runner is None or self.failed is None or self.succeed is None):
             return False
         return True
 
+    def parse(self, data):
+        if data.find("\n") > 0:
+            self.completed = True            
+    
+    def is_completed(self):
+        return self.completed
+    
 def get_socket():
     (af, socktype, proto, canonname, sa) = socket.getaddrinfo(HOST, PORT,
                                                               socket.AF_UNSPEC, socket.SOCK_STREAM,
@@ -26,16 +34,18 @@ def get_socket():
     try:
         sckt = socket.socket(af, socktype, proto)
         sckt.bind(sa)
-        return sckt.listen(1)
+        sckt.listen(1)
+        return sckt
     except socket.error, msg:
         sckt.close()
         sys.exit(1)
 
 
 def listening(conn):
+    mp = MessageParser()
     while 1:
-        data = conn.recv(1024)
-        if not data: return
+        data = conn.recv(512)
+        if not mp.is_valid(): return
         conn.send(data)
 
 def connect():
--- EKG4Test/tests/test_listener.py
+++ EKG4Test/tests/test_listener.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from EKG4Test.listener import MessageParser
 
 class MessageParserTest(TestCase):
-    def test_MessageParser_should_be_ok_when_all_param_are_set(self):
+    def test_is_valid_should_be_ok_when_all_param_are_set(self):
         mp = MessageParser()
         self.assertFalse(mp.is_valid())
         
@@ -17,3 +17,15 @@ class MessageParserTest(TestCase):
 
         mp.succeed = 0
         self.assertTrue(mp.is_valid())
+
+    def test_completed_should_be_true_on_new_line(self):
+        mp = MessageParser()
+        
+        mp.parse("test")
+        self.assertFalse(mp.is_completed())
+        
+        mp.parse("other")
+        self.assertFalse(mp.is_completed())
+        
+        mp.parse("last one\n")
+        self.assertTrue(mp.is_completed())
